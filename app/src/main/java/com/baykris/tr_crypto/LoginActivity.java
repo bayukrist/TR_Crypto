fdf0e6db0505db8985f67375aef41bcb8a1c63c8
bayukrist <672018005@student.uksw.edu>
2021-04-23 23:52:04 +0700

    fix 2

--- app/src/main/java/com/baykris/tr_crypto/LoginActivity.java
+++ app/src/main/java/com/baykris/tr_crypto/LoginActivity.java
@@ -7,16 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.baykris.tr_crypto.ui.home.HomeActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
